0e0cd1fee8a2fc4ef01de8cc4377fd8fe8d83373
Jonas Lacandola <jonaslacandola07@gmail.com>
2026-08-14 23:16:05 +0800

    use English page language

--- app/layout.jsx
+++ app/layout.jsx
@@ -26,7 +26,7 @@ export const viewport = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="fil">
+    <html lang="en-PH">
       <body>{children}</body>
     </html>
   );
